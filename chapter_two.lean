8f1cce7782d596a93474950c0ea78cf96911ffd0
Andrew Ashworth <alashworth@gmail.com>
2016-11-15 17:59:57 -0800

    .

--- chapter_two.lean
+++ chapter_two.lean
@@ -1,64 +1,62 @@
+/-------------------------------------------------------------------------------\
+  Solutions for chapter two: the natural numbers                                  
+  Analysis 1, Third Edition, Tao                                             
+\-------------------------------------------------------------------------------/
+
+-- Lean already contains a basic notion of the natural numbers and arithmetic
+-- operations. There are minor differences, such as its definition of the 
+-- natural numbers being recursive on the second argument. As a result, certain
+-- lemmas or theorems may have their argument positions reversed below.
+
 namespace ch2
+local abbreviation S := nat.succ
+local abbreviation induction_on := @nat.induction_on
 
--- in order to avoid conflicts with lean's built-in nat type,
--- we define the whole numbers
+-- auxiliary lemmas
+lemma add_n_0 (n : ℕ) : n + 0 = n := rfl
 
-inductive whole : Type :=
-| zero : whole
-| succ : whole → whole
+lemma add_n_Sm (m n : ℕ) : n + S m = S (n + m) := rfl
 
-notation 0 := whole.zero
+-- lemma 2.2.2, reversed
+lemma add_0_n (n : ℕ) : 0 + n = n :=
+induction_on n
+  (show 0 + 0 = 0, from add_n_0 0)
+  (take n, 
+    assume ih : 0 + n = n,
+    show 0 + S n = S n, from 
+      calc
+        0 + S n = S (0 + n) : add_n_Sm
+            ... = S n       : ih)
 
-definition plus : whole → whole → whole
-| plus 0 m     := m
-| plus (whole.succ n) m := whole.succ (plus n m)
-
-infix ` + ` := plus
-
-section addition
-
-variables m n : whole
-
-lemma plus_0_n : 0 + n = n := rfl
-lemma plus_Sn_m : whole.succ n + m = whole.succ(n + m) := rfl
-
-lemma plus_n_0 : n + 0 = n := 
-whole.induction_on n 
- (show 0 + 0 = 0, from plus_0_n 0)
-  (take n, assume ih : n + 0 = n, 
-  show whole.succ n + 0 = whole.succ n, from calc
-      whole.succ n + 0 = whole.succ (n + 0) : plus_Sn_m
-                   ... = whole.succ n : ih)
-
-lemma plus_n_Sm : n + whole.succ m = whole.succ (n + m) :=
-begin
-  induction n with n m,
-  rewrite plus_0_n,
-
-  rewrite plus_Sn_m,
-  rewrite m_1
-end
-
-lemma plus_n_Sm : n + whole.succ m = whole.succ (n + m) :=
-whole.induction_on n
-  (show 0 + whole.succ m = whole.succ (0 + m), from calc
-    0 + whole.succ m = whole.succ m : plus_0_n
-                 ... = whole.succ (0 + m) : plus_0_n)
-  (take n, take m, assume ih : n + whole.succ m = whole.succ (n + m),
-  show whole.succ n + whole.succ m = whole.succ ((whole.succ n) + m), from
+-- lemma 2.2.3, reversed
+lemma add_Sn_m (m n : ℕ) : S n + m = S (n + m) :=
+induction_on m
+  (show S n + 0 = S (n + 0), from 
     calc
-      whole.succ n + whole.succ m = whole.succ (whole.succ (n + m)) : plus_Sn_m
-      whole.succ n + whole.succ m = whole.succ (whole.succ (n + m)) : plus_Sn_m
-      whole.succ n + whole.succ m = whole.succ ((whole.succ n) + m) : ih)
---whole.induction_on n
-  --(show 0 + whole.succ m = whole.succ (0 + m), from calc
-    --0 + whole.succ m = whole.succ m : plus_0_n
-    --... = whole.succ (0 + m) : plus_0_n)
-  --(take n m, assume ih : n + whole.succ m = whole.succ (n + m),
-   -- show whole.succ n + whole.succ m = whole.succ (whole.succ n + m), from calc
-     -- whole.succ n + whole.succ m = whole.succ ((whole.succ n) + m) : plus_Sn_m)
+      S n + 0 = S n       : add_n_0
+          ... = S (n + 0) : add_n_0 n)
+  (take m,
+    assume ih : S n + m = S (n + m),
+    show S n + S m = S (n + S m), from
+      calc
+        S n + S m = S (S n + m )  : add_n_Sm
+              ... = S (S (n + m)) : ih
+              ... = S (n + S m)   : add_n_Sm)
 
+-- proposition 2.2.4 
+theorem add_commutative (m n : ℕ) : n + m = m + n :=
+induction_on n
+  (show 0 + m = m + 0, from
+    calc
+      0 + m = m     : add_0_n
+        ... = m + 0 : add_n_0)
+  (take n,
+    assume ih : n + m = m + n,
+    show S n + m = m + S n, from 
+      calc
+        S n + m = S (n + m) : add_Sn_m
+            ... = S (m + n) : ih
+            ... = m + S n : add_n_Sm)
 
-end addition
 
 end ch2
