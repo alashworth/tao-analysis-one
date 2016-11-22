144728a34170f4bf579e48c56b564c25030846ed
Andrew Ashworth <alashworth@gmail.com>
2016-11-21 18:32:41 -0800

    .

--- chapter_two.lean
+++ chapter_two.lean
@@ -3,6 +3,9 @@
   Analysis 1, Third Edition, Tao                                             
 \-------------------------------------------------------------------------------/
 
+import data.nat
+open eq.ops nat
+
 namespace ch2
 local abbreviation S := nat.succ
 local abbreviation induction_on := @nat.induction_on
@@ -18,17 +21,13 @@ lemma zero_not_succ (n : ℕ) : S n = 0 → false :=
 (assume h : S n = 0, show false, from nat.no_confusion h)
 
 -- axiom 2.4 - the successor function is injective
-theorem succ_inj (a b : ℕ) : S a = S b → a = b := 
-assume h : S a = S b,
-  show (a = b), from nat.no_confusion h (λ e : a = b, e)
-
+theorem succ_inj {a b : ℕ} (h : S a = S b) : a = b := 
+  nat.no_confusion h (λ e : a = b, e)
 
 -- auxiliary lemmas
 lemma add_n_0 {n : ℕ} : n + 0 = n := rfl
 
-
 lemma add_n_Sm {n m : ℕ} : n + S m = S (n + m) := rfl
-
 
 -- lemma 2.2.2, reversed
 lemma add_0_n {n : ℕ} : 0 + n = n :=
@@ -55,7 +54,6 @@ induction_on m
         S n + S m = S (S n + m)   : add_n_Sm
               ... = S (S (n + m)) : ih
               ... = S (n + S m)   : add_n_Sm)
-
 
 -- proposition 2.2.4 
 proposition add_comm {n m : ℕ} : n + m = m + n :=
@@ -102,90 +100,63 @@ induction_on a
     proof
       have h1 :   S a + c = S (a + c) , from add_Sn_m,
       have h2 :   S a + b = S (a + b) , from add_Sn_m,
-      have h3 : S (a + b) = S (a + c) , by rewrite [-h2, -h1, h0],
-      have h4 :     a + b = a + c     , by rewrite [-succ_inj, h3],
+      have h3 : S (a + b) = S (a + c) , from h2⁻¹ ⬝ h0  ⬝ h1,
+      have h4 :     a + b = a + c     , from succ_inj h3,
       ih h4
     qed)
-
--- definition 2.2.7
-definition is_positive :  ℕ → Prop
-| (nat.zero  ) := false
-| (nat.succ _) := true
-
--- proposition 2.2.8
-proposition pos_nat_num (a b : ℕ) (h : is_positive a) : is_positive (a + b) :=
-induction_on b
-  (show is_positive (a + 0), by rewrite [add_n_0]; assumption)
-  (take b,
-    assume ih : is_positive (a + b),
-    show is_positive (a + S b), by rewrite [add_n_Sm, ↑is_positive]; trivial)
-
-
--- corollary 2.2.9
-/- suppose that a neq 0, or b neq zero. if a neq 0, then a is positive, and then 
-a + b = 0 is positive by pos_nat_num, a contradiction. a similar argument follows for b -/
-example (a b : ℕ) (h : a + b = 0) : a = 0 ∧ b = 0 :=
-begin
-  apply by_contradiction,
-  rewrite not_and_iff_not_or_not,
-  intro h, cases h with [ha, hb],
-    -- showing false from ¬a = 0
-    -- if a neq 0 (premise), then is_positive a
-    have hapos : is_positive a, begin
-      cases a, 
-        unfold not at ha, 
-        have htriv : (nat.zero = nat.zero), from rfl,
-        have hfalse : false, from ha htriv,
-        unfold is_positive, assumption, 
-
-        rewrite add_Sn_m at h, apply nat.no_confusion h
-    end,
-    -- if is_positive a, then is_positive (a + b)
-    have habpos : is_positive (a + b), begin
-      apply pos_nat_num a b hapos
-    end,
-
-    -- contradiction! 
-    rewrite [h at habpos, ↑is_positive at habpos]; assumption,
-
-    -- showing false from ¬b = 0
-    have hb_pos : is_positive b, begin
-      cases b with b,
-        have (nat.zero = nat.zero), from rfl,
-        apply not.elim hb this,
-
-        rewrite add_n_Sm at h, apply nat.no_confusion h
-    end,
-
-    -- if is_positive b, then is_positive (a + b)
-    have h_abpos : is_positive (a + b), begin
-      rewrite add.comm,  apply pos_nat_num b a hb_pos
-    end,
-
-    -- contradiction!
-    rewrite h at h_abpos, unfold is_positive at h_abpos, assumption 
-end
-
+ 
+-- def 2.2.7, prop 2.2.8 skipped
 
 -- corollary 2.2.9
 example (a b : ℕ) : (a + b = 0) → a = 0 ∧ b = 0 :=
-begin
-  cases a,
-  intro h, split, trivial, rewrite [-h, add_0_n],
-  intro h, rewrite [add_Sn_m at h], apply nat.no_confusion h
-end
+nat.cases_on a
+  (assume h : 0 + b = 0,
+    show (0 = 0 ∧ b = 0), from 
+      and.intro 
+        (show (0 = 0), from rfl) 
+        (show (b = 0), from h ▸ add_0_n⁻¹))
+  (take a,
+    assume h : (S a + b = 0),
+    show (S a = 0 ∧ b = 0), from proof
+      have S (a + b) = 0, from add_Sn_m ▸ h,
+      have false, from nat.no_confusion `S (a + b) = 0`,
+      false.elim this
+    qed)
 
 -- lemma 2.2.10 
-/-
-example : ∀(a : ℕ), is_positive a → ∃!(b : ℕ), S b = a :=
-begin
-  intros a h,
-  induction a with a ih,
-      unfold is_positive at h, exfalso, assumption,
-    repeat split,
-      clear h, intro y h0, rewrite [-succ_inj, h0] 
+example (a b : ℕ) : a = 0 ∨ ∃!b, S b = a :=
+nat.cases_on a
+  (show (0 = 0 ∨ ∃! b, S b = 0), from or.inl (eq.refl 0))
+  (take a,
+    suffices ∃!b, S b = S a, from or.inr this,
+    exists.intro a 
+      (and.intro 
+        (show (S a = S a), from rfl)
+        (take b, 
+          show (S b = S a → b = a), from succ_inj)))
+
+-- proposition 2.2.12 (≥ replaced with ≤ in order to match internal definitions)
+-- order is reflexive
+example (a : ℕ) : a ≤ a := 
+  suffices a + 0 = a, from le.intro this,
+  show (a + 0 = a), from add_n_0
+
+-- order is transitive
+example (a b c : ℕ) (hab : a ≤ b) : b ≤ c →  a ≤ c :=
+le.rec hab (λ p H2, le.step) 
+
+section
+
+variables a b c : ℕ
+premise hab : a ≤ b
+
+check le.rec
+check le.rec hab
+check le.rec hab (λ p hypo, le.step)
+check (λ p hypo, le.step)
+check (le.step)
+
 end
--/
--- definition 2.2.11
+
 
 end ch2
