7049f1d1e29bd8bade876444cf6d7a239d777d3f
Andrew Ashworth <andrew@ashworth.us>
2017-03-22 20:42:41 -0400

    squashme

--- 04_integers_and_rationals.lean
+++ 04_integers_and_rationals.lean
@@ -1,33 +1,32 @@
 namespace integers
 
--- the whole numbers, defined as a cartesian product
-section pre_integers
-
-local notation `whl` := ℕ × ℕ.
-
-def eqv : whl → whl → Prop
-| (a, b) (c, d) := a + d = c + b → (a, b) = (c, d)
+def eqv : ℕ × ℕ → ℕ × ℕ → Prop
+| (a, b) (c, d) := a + d = c + b
 
 infix ` ∽ `:50 := eqv
 
-theorem eqv.refl : ∀ (a : whl), a ∽ a 
-| (a, b) := λ h, rfl.
+theorem eqv.refl : ∀ (a : ℕ × ℕ), a ∽ a 
+| (a, b) := rfl
 
-theorem eqv.symm : ∀ (a b : whl), a ∽ b → b ∽ a
-| (a, b) (c, d) := 
-  assume 
-    (h₁ : a + d = c + b → (a, b) = (c, d))
-    (h₂ : c + b = a + d), 
-  eq.symm (h₁ (eq.symm h₂)).
+theorem eqv.symm : ∀ (a b : ℕ × ℕ), a ∽ b → b ∽ a
+| (a, b) (c, d) := λ h₁, eq.symm h₁
 
-theorem eqv.trans : ∀ (a b c : whl), a ∽ b → b ∽ c → a ∽ c
+theorem eqv.trans : ∀ (a b c : ℕ × ℕ), a ∽ b → b ∽ c → a ∽ c
 | (a, b) (c, d) (e, f) := 
-  assume
-    (h₁ : a + d = c + b → (a, b) = (c, d))
-    (h₂ : c + f = e + d → (c, d) = (e, f))
-    (h₃ : a + f = e + b), 
-  _
-
-end pre_integers
+λ (h₁ : a + d = c + b) 
+  (h₂ : c + f = e + d), 
+show (a + f = e + b), from 
+  have h₃ : a + d + c + f = c + b + e + d, from 
+    calc
+      a + d + c + f = a + d + e + d : by simp [add_assoc, h₂^.symm]
+                ... = c + b + e + d : by simp [add_assoc, h₁^.symm], 
+  have h₄ : c + d + a + f = c + d + b + e, from 
+    calc
+      c + d + a + f = a + d + c + f : by simp [add_comm]
+                ... = c + b + e + d : h₃
+                ... = c + d + b + e : by simp [add_comm],
+  have h₅ : a + f = b + e, 
+    from @add_left_cancel ℕ _ (c + d) _ _ (by simp; simp at h₃; assumption), 
+  h₅^.symm ▸ (add_comm b e ▸ rfl)
 
 end integers
